0394441e17b4b7164e6d3be3aa9a5b770b82fdd1
zhangshengji <961272668@qq.com>
2020-07-29 23:29:36 +0800

    'mybatis_generato'

--- blog-admin/src/main/java/com/zsj/blog/admin/BlogAdminApplication.java
+++ blog-admin/src/main/java/com/zsj/blog/admin/BlogAdminApplication.java
@@ -1,11 +1,40 @@
 package com.zsj.blog.admin;
 
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
-public class BlogAdminApplication {
+public class BlogAdminApplication implements ApplicationRunner {
     public static void main(String[] args) {
         SpringApplication.run(BlogAdminApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+//        generateArtifacts();
+        //生成的数据好像有问题
+    }
+
+    /**
+     * 通过Java code的方式使用mybatis generator
+     * @throws Exception
+     */
+    private void generateArtifacts() throws Exception {
+        List<String> warnings = new ArrayList<>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(
+                this.getClass().getResourceAsStream("/generatorConfig.xml"));
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
     }
 }
